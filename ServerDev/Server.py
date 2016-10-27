5f396b2ede902a9eebd74c1652c70f945983fba4
Nigel <NigelChen@users.noreply.github.com>
2016-10-27 00:37:32 -0600

    Update Server.py

--- ServerDev/Server.py
+++ ServerDev/Server.py
@@ -90,25 +90,6 @@ class server:
 							#it will be the client's responsibility to escape the HTML entities SAFELY.
 							parsed = json.loads(str(self.parseMessage(bytearray(data))))
 							print '[Debug] '+ str(parsed)
-							
-							'''
-								Somebody better fix these damn bugs
-								                                   _
-								       /      \         __      _\( )/_
-								    \  \  ,,  /  /   | /  \ |    /(O)\ 
-								     '-.`\()/`.-'   \_\\  //_/    _.._   _\(o)/_  //  \\
-								    .--_'(  )'_--.   .'/()\'.   .'    '.  /(_)\  _\\()//_
-								   / /` /`""`\ `\ \   \\  //   /   __   \       / //  \\ \
-								    |  |  ><  |  |          ,  |   ><   |  ,     | \__/ |
-								    \  \      /  /         . \  \      /  / .              _
-								   _    '.__.'    _\(O)/_   \_'--`(  )'--'_/     __     _\(_)/_
-								_\( )/_            /(_)\      .--'/()\'--.    | /  \ |   /(O)\
-								 /(O)\  //  \\         _     /  /` '' `\  \  \_\\  //_/
-								       _\\()//_     _\(_)/_    |        |      //()\\ 
-								      / //  \\ \     /(o)\      \      /       \\  //
-								       | \__/ |
-							'''
-							
 							for i in parsed:
 								#NOTE FOR CLIENT REPO: max message length is 256 characters while the avatar, names, etc. max length is 32 characters long
 								#if breaks rules, then the server kicks out the user, indicating that the client tampered with client-side code.
